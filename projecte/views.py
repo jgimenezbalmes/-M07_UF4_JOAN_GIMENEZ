4e9b45842aef97be67d54ff6e915625356cb9e9e
jgimenezbalmes <jgimenez@jaumebalmes.net>
2023-03-23 19:49:30 +0100

    surt un teacher

--- projecte/views.py
+++ projecte/views.py
@@ -5,5 +5,8 @@ from django.template import Context, loader
 # Create your views here.
 def index(request):
     professor = {"name":"Roger", "surname":"Sobrino", "age":"17"}
-    template = loader.get_template('index.html')
-    return HttpResponse(template.render())
+    return render(request, 'index.html', {'nombre':professor["name"], "surname":professor["surname"], "age":professor["age"]})
+
+def teachers(request):
+    professor = {"name":"Roger", "surname":"Sobrino", "age":"17"}
+    return render(request, 'teachers.html', {'nombre':professor["name"], "surname":professor["surname"], "age":professor["age"]})
