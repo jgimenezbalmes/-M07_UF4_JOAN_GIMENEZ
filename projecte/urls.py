100f106b0fa1ec3bf8f8808bf01ada4c087ccc83
jgimenezbalmes <jgimenez@jaumebalmes.net>
2023-03-23 20:37:31 +0100

    fet llistat de profes

--- projecte/urls.py
+++ projecte/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('teachers', views.teachers, name='teachers')
+    path('teachers/', views.teachers, name='teachers')
 ]
--- projecte/views.py
+++ projecte/views.py
@@ -4,9 +4,13 @@ from django.template import Context, loader
 
 # Create your views here.
 def index(request):
-    professor = {"name":"Roger", "surname":"Sobrino", "age":"17"}
+    
     return render(request, 'index.html', {'nombre':professor["name"], "surname":professor["surname"], "age":professor["age"]})
 
+
+professor =[{"name":"Roger", "surname":"Sobrino", "age":"17"}, {"name":"Pere", "surname":"Gobrino", "age":"30"}, {"name":"Soger", "surname":"Nobrino", "age":"71"}]
+
 def teachers(request):
-    professor = {"name":"Roger", "surname":"Sobrino", "age":"17"}
-    return render(request, 'teachers.html', {'nombre':professor["name"], "surname":professor["surname"], "age":professor["age"]})
+   
+    context = {'pr':professor}
+    return render(request, 'teachers.html', context)
