fdab1cb4043dbe1111998b7fdbeba91a0ab446f5
jgimenezbalmes <jgimenez@jaumebalmes.net>
2023-03-30 20:40:53 +0200

    intentant lo del docker

--- projecte/models.py
+++ projecte/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Teacher(models.Model):
+    name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
