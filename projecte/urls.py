61c2c33ef81366818ae892ad8680c22c62cc41ea
jgimenezbalmes <jgimenez@jaumebalmes.net>
2023-04-20 20:36:39 +0200

    fet el crud, pero ara amb students

--- projecte/urls.py
+++ projecte/urls.py
@@ -5,10 +5,13 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('teachers/', views.teachers_llista, name='teachers_llista'),
     path('teachers/teacher/<str:pk>/', views.teacher_agafaun, name='teacher_un'),
-    path('students/', views.students, name='students'),
-    path('students/student/<str:pk>/', views.student, name='student'),
+    path('students/', views.students_llista, name='students_llista'),
+    path('students/student/<str:pk>/', views.student_agafaun, name='student_un'),
     #path('user-form/', views.user_form, name='user_form'),
     path('teacher-create/', views.teacher_form, name='teacher_form'),
     path('update-teacher/<str:pk>/', views.update_teacher, name='update-teacher'),
-    path('delete-teacher/<str:pk>/', views.delete_teacher, name='delete-teacher')
+    path('delete-teacher/<str:pk>/', views.delete_teacher, name='delete-teacher'),
+    path('student-create/', views.student_form, name='student_form'),
+    path('update-student/<str:pk>/', views.update_student, name='update-student'),
+    path('delete-student/<str:pk>/', views.delete_student, name='delete-student'),
 ]
--- projecte/views.py
+++ projecte/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import Context, loader
-from .models import Teacher
-from .forms import TeacherForm
+from .models import Teacher, Student
+from .forms import TeacherForm, StudentForm
 
 
 # Create your views here.
@@ -25,9 +25,19 @@ def teacher_agafaun(request, pk):
     context = {'teacher':teacher}
     return render(request, 'teacher.html', context)
 
-def students(request):
-    context = {'st':student}
+def students_llista(request):
+    students = Student.objects.all()
+    context = {'students':students}
     return render(request, 'students.html', context)
+
+def student_agafaun(request, pk):
+    student = Student.objects.get(id=pk)
+    context = {'student':student}
+    return render(request, 'student.html', context)
+
+# def students(request):
+#     context = {'st':student}
+#     return render(request, 'students.html', context)
 
 # def teacher(request, pk):
 #     teacher_Obj = None
@@ -36,12 +46,12 @@ def students(request):
 #             teacher_Obj = i
 #     return render(request, 'teacher.html', {'teachers': teacher_Obj})
 
-def student(request, pk):
-    student_Obj = None
-    for i in student:
-        if i['id'] == pk:
-            student_Obj = i
-    return render(request, 'student.html', {'students': student_Obj})
+# def student(request, pk):
+#     student_Obj = None
+#     for i in student:
+#         if i['id'] == pk:
+#             student_Obj = i
+#     return render(request, 'student.html', {'students': student_Obj})
 
 # def user_form(request):
 #     form = TeacherForm()
@@ -81,4 +91,39 @@ def delete_teacher(request, pk):
         return redirect('teachers_llista')
 
     context = {'object':teacher}
+    return render(request, 'delete_object.html', context)
+
+def student_form(request):
+    form = StudentForm()
+
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+
+            form.save()
+            return redirect('students_llista')
+    context = {'form':form}
+    return render(request, 'form_student.html', context)
+
+def update_student(request, pk):
+    student = Student.objects.get(id=pk)
+    form = StudentForm(instance=student)
+
+    if request.method == 'POST':
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect('students_llista')
+    
+    context = {'form':form}
+    return render(request, 'form.html', context)
+
+def delete_student(request, pk):
+    student = Student.objects.get(id = pk)
+
+    if request.method == 'POST':
+        student.delete()
+        return redirect('students_llista')
+
+    context = {'object':student}
     return render(request, 'delete_object.html', context)
